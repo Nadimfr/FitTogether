386c089f6b17410ca9c5c07345e89363266852cb
Nadimfr <nadim.farah@hotmail.com>
2022-05-23 23:32:24 +0300

    Nadim

--- src/Screens/Shop/shop.js
+++ src/Screens/Shop/shop.js
@@ -1,24 +1,34 @@
 import React, { useEffect, useState } from "react";
 import "../Shop/shop.scss";
 import ItemsCarousel from "react-items-carousel";
-import Dumbells from "../../Assets/Images/Dumbells.png";
-import Drink from "../../Assets/Images/drink.png";
-import Package from "../../Assets/Images/package.jpg";
+
 import { AiOutlineRight, AiOutlineLeft } from "react-icons/ai";
 import * as ProductsApi from "../../Core/apis/products";
 import Countdown from "react-countdown";
 import { Link } from "react-router-dom";
 
 const Shop = () => {
-  const [products, setProducts] = useState([]);
+  const [gymtools, setGymtools] = useState([]);
+  const [drinks, setDrinks] = useState([]);
+  const [packages, setPackages] = useState([]);
   const [activeItemIndex, setActiveItemIndex] = useState(0);
   const [activeItemIndex1, setActiveItemIndex1] = useState(0);
   const [activeItemIndex2, setActiveItemIndex2] = useState(0);
   const chevronWidth = 40;
 
   useEffect(() => {
-    ProductsApi.getAllProducts().then((res) => {
-      setProducts(res);
+    ProductsApi.getGymTools().then((res) => {
+      setGymtools(res);
+      console.log(res);
+    });
+
+    ProductsApi.getDrinks().then((res) => {
+      setDrinks(res);
+      console.log("Fitness", res);
+    });
+
+    ProductsApi.getPackages().then((res) => {
+      setPackages(res);
       console.log(res);
     });
   }, []);
@@ -31,14 +41,14 @@ const Shop = () => {
           <ItemsCarousel
             requestToChangeActive={setActiveItemIndex}
             activeItemIndex={activeItemIndex}
-            numberOfCards={window.innerWidth > "700px" ? 4 : 1}
+            numberOfCards={window.innerWidth > 700 ? 4 : 1}
             gutter={20}
             leftChevron={<AiOutlineLeft size={30} color="white" />}
             rightChevron={<AiOutlineRight size={30} color="white" />}
             outsideChevron
             chevronWidth={chevronWidth}
           >
-            {products.map((product) => (
+            {gymtools.map((product) => (
               <Link
                 to={`/product-details/${product._id}`}
                 style={{ textDecoration: "none", color: "black" }}
@@ -59,48 +69,25 @@ const Shop = () => {
           <ItemsCarousel
             requestToChangeActive={setActiveItemIndex1}
             activeItemIndex={activeItemIndex1}
-            numberOfCards={window.innerWidth > "700px" ? 4 : 1}
+            numberOfCards={window.innerWidth > 700 ? 4 : 1}
             gutter={20}
             leftChevron={<AiOutlineLeft size={30} color="white" />}
             rightChevron={<AiOutlineRight size={30} color="white" />}
             outsideChevron
             chevronWidth={chevronWidth}
           >
-            <div className="card">
-              <img className="product-image" src={Drink} />
-              <div className="product-title">Muscle Blaze Weigh Active</div>
-              <div className="product-price">65.00 $</div>
-            </div>
-            <div className="card">
-              <img className="product-image" src={Drink} />
-              <div className="product-title">Muscle Blaze Weigh Active</div>
-              <div className="product-price">65.00 $</div>
-            </div>
-            <div className="card">
-              <img className="product-image" src={Drink} />
-              <div className="product-title">Muscle Blaze Weigh Active</div>
-              <div className="product-price">65.00 $</div>
-            </div>
-            <div className="card">
-              <img className="product-image" src={Drink} />
-              <div className="product-title">Muscle Blaze Weigh Active</div>
-              <div className="product-price">65.00 $</div>
-            </div>
-            <div className="card">
-              <img className="product-image" src={Drink} />
-              <div className="product-title">Muscle Blaze Weigh Active</div>
-              <div className="product-price">65.00 $</div>
-            </div>
-            <div className="card">
-              <img className="product-image" src={Drink} />
-              <div className="product-title">Muscle Blaze Weigh Active</div>
-              <div className="product-price">65.00 $</div>
-            </div>
-            <div className="card">
-              <img className="product-image" src={Drink} />
-              <div className="product-title">Muscle Blaze Weigh Active</div>
-              <div className="product-price">65.00 $</div>
-            </div>
+            {drinks.map((product) => (
+              <Link
+                to={`/product-details/${product._id}`}
+                style={{ textDecoration: "none", color: "black" }}
+              >
+                <div className="card">
+                  <img className="product-image" src={product.image} />
+                  <div className="product-title">{product.name}</div>
+                  <div className="product-price">{product.price} $</div>
+                </div>
+              </Link>
+            ))}
           </ItemsCarousel>
         </div>
       </div>
@@ -115,53 +102,25 @@ const Shop = () => {
           <ItemsCarousel
             requestToChangeActive={setActiveItemIndex2}
             activeItemIndex={activeItemIndex2}
-            numberOfCards={window.innerWidth > "700px" ? 4 : 1}
+            numberOfCards={window.innerWidth > 700 ? 4 : 1}
             gutter={20}
             leftChevron={<AiOutlineLeft size={30} color="white" />}
             rightChevron={<AiOutlineRight size={30} color="white" />}
             outsideChevron
             chevronWidth={chevronWidth}
           >
-            <div className="card">
-              <img
-                src={Package}
-                style={{ marginBottom: "0px", width: "70%" }}
-              />
-              <div className="product-title">Summer Package 1</div>
-              <div className="product-price">1,099.00 $</div>
-            </div>
-            <div className="card">
-              <img
-                src={Package}
-                style={{ marginBottom: "0px", width: "70%" }}
-              />
-              <div className="product-title">Summer Package 1</div>
-              <div className="product-price">1,099.00 $</div>
-            </div>
-            <div className="card">
-              <img
-                src={Package}
-                style={{ marginBottom: "0px", width: "70%" }}
-              />
-              <div className="product-title">Summer Package 1</div>
-              <div className="product-price">1,099.00 $</div>
-            </div>
-            <div className="card">
-              <img
-                src={Package}
-                style={{ marginBottom: "0px", width: "70%" }}
-              />
-              <div className="product-title">Summer Package 1</div>
-              <div className="product-price">1,099.00 $</div>
-            </div>
-            <div className="card">
-              <img
-                src={Package}
-                style={{ marginBottom: "0px", width: "70%" }}
-              />
-              <div className="product-title">Summer Package 1</div>
-              <div className="product-price">1,099.00 $</div>
-            </div>
+            {packages.map((product) => (
+              <Link
+                to={`/product-details/${product._id}`}
+                style={{ textDecoration: "none", color: "black" }}
+              >
+                <div className="card">
+                  <img className="product-image" src={product.image} />
+                  <div className="product-title">{product.name}</div>
+                  <div className="product-price">{product.price} $</div>
+                </div>
+              </Link>
+            ))}
           </ItemsCarousel>
         </div>
       </div>
